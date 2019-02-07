48280f4abbd4c9414355c1e136b0b4620e9a2f64
BONNEVILLE Geoffroy <gbonneville@mega.com>
2019-02-07 17:37:09 +0100

    Update API tests

--- CatMash.Tests/ApiTests.cs
+++ CatMash.Tests/ApiTests.cs
@@ -1,16 +1,26 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using CatMash.API.Controllers;
+using Microsoft.Extensions.Configuration;
+using Moq;
 
 namespace CatMash.Tests
 {
     public class ApiTests
     {
         [Fact]
-        public void TestGetVotingObjects()
+        public void TestGetImagesWithNoUrlFails()
         {
-            var apiController = new ImagesController();
-            var votingObjects = apiController.GetVotingObjects();
-            Assert.NotNull(votingObjects);
+            var apiController = new ImagesController(Mock.Of<IConfiguration>());
+            Assert.ThrowsAsync<UriFormatException>(async () => await apiController.GetImages());
+        }
+
+        [Fact]
+        public void TestGetImagesWithValidUrlSucceeds()
+        {
+            var apiController = new ImagesController(Mock.Of<IConfiguration>(c => c["SourceImagesUrl"] == "https://latelier.co/data/cats.json"));
+            var images = apiController.GetImages().Result;
+            Assert.NotEmpty(images);
         }
     }
 }
