9373b6a334c3f95c54a77bab3af0c325870f34fa
BONNEVILLE Geoffroy <gbonneville@mega.com>
2019-02-07 16:09:34 +0100

    API uses configuration service for URL

--- CatMash.API/Controllers/ImagesController.cs
+++ CatMash.API/Controllers/ImagesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CatMash.API.Interfaces;
 using CatMash.API.Repositories;
+using Microsoft.Extensions.Configuration;
 
 namespace CatMash.API.Controllers
 {
@@ -12,12 +13,19 @@ namespace CatMash.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly string _sourceUrl;
+
+        public ImagesController(IConfiguration config)
+        {
+            _sourceUrl = config["SourceImagesUrl"];
+        }
+
         public async Task<IEnumerable<IImage>> GetImages()
         {
             using (var client = new HttpClient())
             {
                 var serializer = new DataContractJsonSerializer(typeof(ImageRepository));
-                var streamTask = client.GetStreamAsync("https://latelier.co/data/cats.json");
+                var streamTask = client.GetStreamAsync(_sourceUrl);
                 var images = serializer.ReadObject(await streamTask) as ImageRepository;
                 return images?.Images;
             }
