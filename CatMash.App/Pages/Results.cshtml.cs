9d974e027998bf6480f697bff38fffd0dc8815f7
BONNEVILLE Geoffroy <gbonneville@mega.com>
2019-02-07 10:58:31 +0100

    Results page is working

---
+++ CatMash.App/Pages/Results.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatMash.App.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatMash.App.Pages
+{
+    public class ResultsModel : PageModel
+    {
+        private readonly IImageService _imageService;
+
+        [BindProperty]
+        public IEnumerable<IImage> Images { get; private set; }
+
+        public ResultsModel(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+        public void OnGet()
+        {
+            Images = _imageService.Images.OrderByDescending(i => i.Votes);
+        }
+    }
+}
