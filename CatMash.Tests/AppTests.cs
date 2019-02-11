db6a2be692ba6e2bb2f82daf1d473f7c37b05096
BONNEVILLE Geoffroy <gbonneville@mega.com>
2019-02-11 11:51:07 +0100

    Unit tests finally working Added test for voting service

--- CatMash.Tests/AppTests.cs
+++ CatMash.Tests/AppTests.cs
@@ -1,14 +1,39 @@
-﻿using CatMash.App.Services;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CatMash.App.Interfaces;
+using CatMash.App.Models;
+using CatMash.App.Services;
+using Moq;
 using Xunit;
 
 namespace CatMash.Tests
 {
     public class AppTests
     {
-        [Fact]
-        public void TestImageService()
+        private readonly List<IImage> _images = new List<IImage>() {new Image {Id = "id1"}, new Image {Id = "id2"}, new Image { Id = "id3" }, new Image { Id = "id4" } };
+        private readonly IImageService _imageService;
+
+        public AppTests()
         {
-            
+            _imageService = Mock.Of<IImageService>(i => i.Images == _images);
+        }
+
+        [Fact]
+        public void TestVotingServiceGetImagesShouldBeDifferent()
+        {
+            var votingService = new VotingService(_imageService);
+            var images = votingService.GetImagesToCompare().ToList();
+            Assert.Equal(2, images.Count());
+            Assert.NotEqual(images[0].Id, images[1].Id);
+        }
+
+        [Fact]
+        public void TestVotingServiceIncreaseVoteCount()
+        {
+            var votingService = new VotingService(_imageService);
+            var images = votingService.GetImagesToCompare().ToList();
+            votingService.Vote(images[0].Id);
+            Assert.Equal(1, images[0].Votes);
         }
     }
 }
