8997aeb1f9fc376bff743333285c7057b2642b49
BONNEVILLE Geoffroy <gbonneville@mega.com>
2019-02-11 12:00:16 +0100

    Code cleanup

--- CatMash.App/Startup.cs
+++ CatMash.App/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CatMash.App.Interfaces;
 using CatMash.App.Services;
 using Microsoft.AspNetCore.Builder;
